cc3122ba3bfab43b287d79264810c131eb7755cb
Yara Megawer <108768001+yaramegawer@users.noreply.github.com>
2026-02-11 21:09:02 +0200

    Update Section 2 Topic 1.sql

--- Library/Section 2 Topic 1.sql
+++ Library/Section 2 Topic 1.sql
@@ -5,3 +5,26 @@ SELECT COUNT(*) AS "عدد الكتب" FROM Books;
 -- عدد الكتب المستعارة حالياً
 SELECT COUNT(*)
 FROM Borrowed_Books
+
+
+SELECT SUM(price)
+FROM Books;
+
+
+
+SELECT AVG(price) 
+FROM Books;
+	
+
+
+SELECT MIN(price) 
+FROM Books;
+	
+
+
+SELECT MAX(price) 
+FROM Books;
+
+
+
+
